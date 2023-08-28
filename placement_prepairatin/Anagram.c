74905a5e53c5585ec92622ab3641a6387046b6f2
Jyoti9988 <120414736+Jyoti9988@users.noreply.github.com>
2023-08-28 11:01:38 -0700

    Add files via upload

---
+++ placement_prepairatin/Anagram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int is_anagram(char a[],char b[]){
+    if(strlen(a)!=strlen(b)){
+      return 0;
+    }
+    int i=0;
+    int freq_a[26]={0};
+    int freq_b[26]={0};
+}
+int main(){
+
+}
